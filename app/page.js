45f959394f0b9c321ddcfa07bcf8c7adbc0f752c
Shiuan <berserker850330@gmail.com>
2024-07-08 15:38:11 +0800

    data update

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           <div className={`${styles.container}`}>
             <div className={`${styles.row} ${styles.alignItemCenter}`}>
               <div className={styles.homeText}>
-                <p>{`Hello, I&apos;m`}</p>
+                <p>{`Hello, I'm`}</p>
                 <h1>Yao Shiuan</h1>
                 <h2>frontend wed developer</h2>
                 <Link
--- app/profolio/portfolio.js
+++ app/profolio/portfolio.js
@@ -2,6 +2,16 @@ export const data = [
   {
     portfolio: [
       {
+        id: 3,
+        img: "/img/portfolio/4.jpg",
+        title: "GBF Game Web Design",
+        des: "The website is mainly designed to design GBF game 3D carousel and Cyberpunk background.",
+        date: "15 June 2024",
+        used: "Html, Css ,javascript",
+        role: "Frontend",
+        view: "https://gbf-slider3d.vercel.app/",
+      },
+      {
         id: 0,
         img: "/img/portfolio/1.png",
         title: "My Weather App",
@@ -25,21 +35,11 @@ export const data = [
         id: 2,
         img: "/img/portfolio/3.jpg",
         title: "i'm shiuan",
-        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Minima esse perferendis corporis animi vero ipsa asperioresquae deleniti soluta sapiente aspernatur tempora, non nisidolorem! Iusto expedita, officiis rerum doloremque, nostrumdignissimos quidem quae sequi possimus voluptate eum doloremlibero.",
+        des: "This is a website about me.",
         date: "4 Dec 2020",
         used: "Html, Css ,javascript",
         role: "Frontend",
-        view: "www.domain.com",
-      },
-      {
-        id: 3,
-        img: "/img/portfolio/4.jpg",
-        title: "GBF Game Web Design",
-        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Minima esse perferendis corporis animi vero ipsa asperioresquae deleniti soluta sapiente aspernatur tempora, non nisidolorem! Iusto expedita, officiis rerum doloremque, nostrumdignissimos quidem quae sequi possimus voluptate eum doloremlibero.",
-        date: "4 Dec 2020",
-        used: "Html, Css ,javascript",
-        role: "Frontend",
-        view: "https://gbf-slider3d.vercel.app/",
+        view: "https://shiuan-portfolio.vercel.app/profolio",
       },
       {
         id: 4,
